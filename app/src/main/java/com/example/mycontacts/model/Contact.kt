865d4103858e63b31ce8c1000ae8036e677aa519
Pedro Miguel Pérez Torres <pmpedrotorres@gmail.com>
2023-11-16 11:12:33 -0600

    Added FireStoreFunctions

---
+++ app/src/main/java/com/example/mycontacts/model/Contact.kt
@@ -0,0 +1,12 @@
+package com.example.mycontacts.model
+
+data class Contact(
+    val name: String,
+    val paternalSurname: String,
+    val maternalSurname: String,
+    val age: Int,
+    val number: Int,
+    val gender: Int,
+    val imageUrl: String,
+    val email: String
+)
