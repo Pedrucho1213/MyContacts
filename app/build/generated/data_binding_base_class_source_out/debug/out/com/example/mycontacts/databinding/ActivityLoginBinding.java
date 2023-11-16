0a2a5b9c990556b9bee32a6af902d68dadd6b4c0
Pedro Miguel Pérez Torres <pmpedrotorres@gmail.com>
2023-11-16 10:18:40 -0600

    Added login activity and cards contacts compoenent

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/example/mycontacts/databinding/ActivityLoginBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/example/mycontacts/databinding/ActivityLoginBinding.java
@@ -4,20 +4,46 @@ package com.example.mycontacts.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import com.example.mycontacts.R;
+import com.google.android.material.textfield.TextInputLayout;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class ActivityLoginBinding implements ViewBinding {
   @NonNull
   private final ConstraintLayout rootView;
 
-  private ActivityLoginBinding(@NonNull ConstraintLayout rootView) {
+  @NonNull
+  public final TextInputLayout emailTxt;
+
+  @NonNull
+  public final Button googleBtn;
+
+  @NonNull
+  public final Button loginBtn;
+
+  @NonNull
+  public final TextInputLayout passTxt;
+
+  @NonNull
+  public final Button registerBtn;
+
+  private ActivityLoginBinding(@NonNull ConstraintLayout rootView,
+      @NonNull TextInputLayout emailTxt, @NonNull Button googleBtn, @NonNull Button loginBtn,
+      @NonNull TextInputLayout passTxt, @NonNull Button registerBtn) {
     this.rootView = rootView;
+    this.emailTxt = emailTxt;
+    this.googleBtn = googleBtn;
+    this.loginBtn = loginBtn;
+    this.passTxt = passTxt;
+    this.registerBtn = registerBtn;
   }
 
   @Override
@@ -43,10 +69,44 @@ public final class ActivityLoginBinding implements ViewBinding {
 
   @NonNull
   public static ActivityLoginBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.email_txt;
+      TextInputLayout emailTxt = ViewBindings.findChildViewById(rootView, id);
+      if (emailTxt == null) {
+        break missingId;
+      }
 
-    return new ActivityLoginBinding((ConstraintLayout) rootView);
+      id = R.id.google_btn;
+      Button googleBtn = ViewBindings.findChildViewById(rootView, id);
+      if (googleBtn == null) {
+        break missingId;
+      }
+
+      id = R.id.login_btn;
+      Button loginBtn = ViewBindings.findChildViewById(rootView, id);
+      if (loginBtn == null) {
+        break missingId;
+      }
+
+      id = R.id.pass_txt;
+      TextInputLayout passTxt = ViewBindings.findChildViewById(rootView, id);
+      if (passTxt == null) {
+        break missingId;
+      }
+
+      id = R.id.register_btn;
+      Button registerBtn = ViewBindings.findChildViewById(rootView, id);
+      if (registerBtn == null) {
+        break missingId;
+      }
+
+      return new ActivityLoginBinding((ConstraintLayout) rootView, emailTxt, googleBtn, loginBtn,
+          passTxt, registerBtn);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
